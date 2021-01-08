57f66e48d14b6d8573faafbef04b0feb5ae469ee
meikai <meikai@wumii.com>
2021-01-08 11:18:07 +0800

    init from https://start.spring.io/

---
+++ src/main/java/com/example/amisdemo/AmisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.amisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmisDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/amisdemo/AmisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
